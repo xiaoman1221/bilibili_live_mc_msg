f6373403022c9072879f7ca9283e9c365b7483bd
小满1221 <1467510054@qq.com>
2022-01-17 00:31:08 +0800

    哼哼啊啊啊啊啊啊啊啊

--- bilibili_live_mc_msg/__init__.py
+++ bilibili_live_mc_msg/__init__.py
@@ -12,16 +12,33 @@ import blivedm
 #当插件被加载时(on_load)
 def on_load(server: PluginServerInterface, old):
     server.logger.info(server.tr('bilibili_live_mc_msg.load_message'))
-    server.logger.info(main())
 #强缩进，注意空格！
 
+#当服务器停止的时候
+def on_server_stop(server: PluginServerInterface, server_return_code: int):
+    if server_return_code != 0:
+        asyncio.get_event_loop().run_until_complete(main())
+
+
+#config_path = '../config/xiaoman1221_blh/config.json'
+#config_file = 'config.json'
 
 # 直播间ID的取值看直播间URL
 TEST_ROOM_IDS = [
     7777,
     
 ]
+
+
+async def main():
+    await run_single_client()
+    await run_multi_client()
+
+
 async def run_single_client():
+    """
+    演示监听一个直播间
+    """
     room_id = random.choice(TEST_ROOM_IDS)
     # 如果SSL验证失败就把ssl设为False，B站真的有过忘续证书的情况
     client = blivedm.BLiveClient(room_id, ssl=True)
@@ -30,11 +47,35 @@ async def run_single_client():
 
     client.start()
     try:
+        # 演示5秒后停止
         await asyncio.sleep(5)
         client.stop()
+
         await client.join()
     finally:
         await client.stop_and_close()
+
+
+async def run_multi_client():
+    """
+    演示同时监听多个直播间
+    """
+    clients = [blivedm.BLiveClient(room_id) for room_id in TEST_ROOM_IDS]
+    handler = MyHandler()
+    for client in clients:
+        client.add_handler(handler)
+        client.start()
+
+    try:
+        await asyncio.gather(*(
+            client.join() for client in clients
+        ))
+    finally:
+        await asyncio.gather(*(
+            client.stop_and_close() for client in clients
+        ))
+
+
 class MyHandler(blivedm.BaseHandler):
     # # 演示如何添加自定义回调
     # _CMD_CALLBACK_DICT = blivedm.BaseHandler._CMD_CALLBACK_DICT.copy()
@@ -61,5 +102,4 @@ class MyHandler(blivedm.BaseHandler):
     async def _on_super_chat(self, client: blivedm.BLiveClient, message: blivedm.SuperChatMessage):
         print(f'[{client.room_id}] 醒目留言 ¥{message.price} {message.uname}：{message.message}')
 
-async def main():
-    await run_single_client()
+
