03e719f1ecd57634abc54c3401d236038217d807
小满1221 <1467510054@qq.com>
2022-01-16 23:03:55 +0800

    前期套轮子

--- bilibili_live_mc_msg/__init__.py
+++ bilibili_live_mc_msg/__init__.py
@@ -1,7 +1,13 @@
+# -*- coding: utf-8 -*-
+
 #加载所有API
 from mcdreforged.api.all import *
 #加载库文件
 from bilibili_live_mc_msg import libs
+#加载依赖
+import asyncio
+import random
+import blivedm
 
 #当插件被加载时(on_load)
 def on_load(server: PluginServerInterface, old):
