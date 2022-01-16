3c30443cc2daf2b0800c80111c57f2a192236462
小满1221 <1467510054@qq.com>
2022-01-16 22:22:14 +0800

    test
    test

--- bilibili_live_mc_msg/__init__.py
+++ bilibili_live_mc_msg/__init__.py
@@ -1,7 +1,9 @@
+#加载所有API
 from mcdreforged.api.all import *
-
+#加载库文件
 from bilibili_live_mc_msg import libs
 
-
+#当插件被加载时(on_load)
 def on_load(server: PluginServerInterface, old):
     server.logger.info(server.tr('bilibili_live_mc_msg.load_message'))
+#强缩进，主要空格！
