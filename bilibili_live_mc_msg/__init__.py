292b8fdf937721580a21f2ea494d0d7207d94406
小满1221 <1467510054@qq.com>
2022-01-16 23:11:59 +0800

    尝试修复bug
    尝试修复bug

--- bilibili_live_mc_msg/__init__.py
+++ bilibili_live_mc_msg/__init__.py
@@ -7,7 +7,7 @@ from bilibili_live_mc_msg import libs
 #加载依赖
 import asyncio
 import random
-import blivedm
+from blivedm import blivedm
 
 #当插件被加载时(on_load)
 def on_load(server: PluginServerInterface, old):
