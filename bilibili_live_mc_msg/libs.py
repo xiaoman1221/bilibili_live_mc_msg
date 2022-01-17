3bb078a53c9dae72a4f03b0410f2fd8d1c8a5df6
小满1221 <1467510054@qq.com>
2022-01-17 17:10:23 +0800

    咕咕咕
    老鸽子的爱

--- bilibili_live_mc_msg/libs.py
+++ bilibili_live_mc_msg/libs.py
@@ -1,3 +1,57 @@
-#库们
-def load():
-	pass
+# -*- coding: utf-8 -*-
+
+
+#加载API
+from mcdreforged.api.all import *
+#加载依赖
+import asyncio
+import blivedm
+import random
+
+
+# 直播间ID的取值,看直播间URL
+# 例子：https://live.bilibili.com/13007212,其中13007212就是直播间ID
+# 支持多个直播间轮询
+ROOM_IDS = [
+    23367128,
+    13007212
+]
+async def run_single_client():
+    """
+    监听直播间
+    """
+    room_id = random.choice(ROOM_IDS)
+    # 如果SSL验证失败就把ssl设为False，B站真的有过忘续证书的情况
+    client = blivedm.BLiveClient(room_id, ssl=True)
+    handler = MyHandler()
+    client.add_handler(handler)
+
+    client.start()
+    try:
+        # 5秒后停止
+        await asyncio.sleep(5)
+        client.stop()
+
+        await client.join()
+    finally:
+        await client.stop_and_close()
+class MyHandler(blivedm.BaseHandler):
+
+    async def _on_heartbeat(self, client: blivedm.BLiveClient, message: blivedm.HeartbeatMessage):
+        print(f'[{client.room_id}] 当前人气值：{message.popularity}')
+
+    async def _on_danmaku(self, client: blivedm.BLiveClient, message: blivedm.DanmakuMessage):
+        print(f'[{client.room_id}] {message.uname}：{message.msg}')
+
+    async def _on_gift(self, client: blivedm.BLiveClient, message: blivedm.GiftMessage):
+        print(f'[{client.room_id}] {message.uname} 赠送{message.gift_name}x{message.num}'
+              f' （{message.coin_type}瓜子x{message.total_coin}）')
+
+    async def _on_buy_guard(self, client: blivedm.BLiveClient, message: blivedm.GuardBuyMessage):
+        print(f'[{client.room_id}] {message.username} 购买{message.gift_name}')
+
+    async def _on_super_chat(self, client: blivedm.BLiveClient, message: blivedm.SuperChatMessage):
+        print(f'[{client.room_id}] 醒目留言 ¥{message.price} {message.uname}：{message.message}')
+
+async def main():
+    await run_single_client()
